d40080248c15f69874fa01b5521d93702d4415bf
huohuo <2991297756@qq.com>
2019-01-14 17:28:42 +0800

    add ubuntu mount remote dir cmd

---
+++ note/mount_remote_directory.sh
@@ -0,0 +1,2 @@
+sudo mount -t cifs -o username=drv2,password=drv2,dir_mode=0777,file_mode=0777 //172.19.1.157/hzn /home/han/drv2/
+sudo mount -t cifs -o username=drv2,password=drv2,dir_mode=0777,file_mode=0777 //172.19.1.157/hzn2 /home/han/hzn2/
